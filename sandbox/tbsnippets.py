edf134645d5bc5a6c1346bdd65ddc5b337254592
dl21273@essex.ac.uk <dleece@firstfiretech.ca>
2022-02-19 11:29:25 -0700

    changed to deal with bytes on file import, fixed time checking

--- sandbox/tbsnippets.py
+++ sandbox/tbsnippets.py
@@ -18,23 +18,23 @@ import time, datetime, sys, os
 # This has time and memory consumption impacts as well as error handling requirements ( data type checking)
 
 def file2mem(filepath):
-    start = datetime.time()
+    start = time.time()
     # Get file on disk size and cap at 16 meg for now
     filesize = os.stat(filepath).st_size
     if (filesize > (16 * 1000 * 1024) ):
         print("file exceeded current limit\n")
         exit(1)
-    # Read file into memory buffer
-    with open(filepath, 'r') as fh:
+    # Read file into memory buffer as bytes rather than encoded string
+    with open(filepath, 'rb') as fh:
         filecontent = fh.read()   # reads whole file
-    stop = datetime.time()
+    stop = time.time()
     
     # output stats
     print("File size on disk is {} bytes\n".format(filesize))
-    print("File loading time was {} microseconds\n".format(stop - start))
+    print("File loading time was {} seconds\n".format(stop - start))
     bytesused=sys.getsizeof(filecontent)
     print("File size in memory is {} bytes\n".format(bytesused))
-    print("Memory object is: {}\n".format(type(filecontent)))
+    print("Memory object data type is: {}\n".format(type(filecontent)))
     
     # Clean up the object
     del filecontent
