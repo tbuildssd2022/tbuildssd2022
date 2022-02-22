4d676715e3a4054c653412119505a789aca4d3c8
dl21273@essex.ac.uk <dleece@firstfiretech.ca>
2022-02-22 07:28:04 -0700

    testing datatype output from sqlalchemy

--- app/sfrapp/main.py
+++ app/sfrapp/main.py
@@ -151,6 +151,7 @@ def login2():
     if accessid and passwd:
         uid= DataUser.query.filter(DataUser.useraccessid==accessid).first()
     print(uid)
+    print(type(uid))
     #if current_user.is_authenticated:
     #   sessioncid=current_user.get_id()
     #    account=User.query.filter_by(id=sessioncid).first()
