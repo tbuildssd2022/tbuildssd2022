321ffdd311e18ba3cdd093849a865627b4d26e73
dl21273@essex.ac.uk <dleece@firstfiretech.ca>
2022-02-22 23:46:08 -0700

    fixed up auth and index to post to login

--- app/sfrapp/auth.py
+++ app/sfrapp/auth.py
@@ -1,3 +1,4 @@
+from crypt import methods
 import flask
 #import flask_login
 from flask import Blueprint, render_template, redirect, url_for,flash, request
@@ -16,14 +17,53 @@ from urllib.parse import urlparse, urljoin
 
 auth = Blueprint('auth', __name__)
 
+
+#############################################   Move to module ####################################
+# Move all this to a seperate module so we can valdiate all three things, pwdcheck should be last
+def getdatauser(aid):
+    duserobj= DataUser.query.filter(DataUser.useraccessid==aid).first()
+    return duserobj
+    
+def getauthnz(uidint):
+    uauthznobj=User.query.filter(User.id==uidint).first()
+    return uauthznobj
+
+def verify_passwd(pwdhash,pwdstr):
+    return check_password_hash(pwdhash,pwdstr)
+
+#########################################################################################################
+
+
 @auth.route('/login', methods=['POST'])
 def login_post():
     accessid = request.form.get('accessid')
-    passwd = request.form.get('passwd')
+    formpasswd = request.form.get('passwd')
     print(accessid)
-    print(passwd)
+    print(formpasswd)
+    # Need form validation on the accessid field.
+    if accessid and formpasswd:
+        thisduserobj=getdatauser(accessid)
+        # Catches invalid user
+        if thisduserobj is None:
+            return redirect(url_for('main.index'))
+        # Check password
+        pwdchk=False # ensure authz check comes back true before proceeding
+        if isinstance(thisduserobj.userid,int):
+            thisauthzobj=getauthnz(thisduserobj.userid)
+        if thisauthzobj is not None:
+            pwdchk=verify_passwd(thisauthzobj.userpasswd, formpasswd)
+        else:
+            return redirect(url_for('main.index'))
+        if pwdchk:
+            print("Setup login manager for this user")
+            login_user(thisauthzobj)
+            return redirect(url_for('main.presenthome'))        
+        else:
+            print("passwordcheck failed")
+            return redirect(url_for('main.index'))
+
     #return render_template('home.html')
-    return redirect(url_for('main.presenthome'))
+    #return redirect(url_for('main.presenthome'))
     # figure out the correct query here, may need to pull UID from access ID first then check password
     #account=User.query.filter_by(aid=accessid).first()
     #if not account or not check_password_hash(account.pwd, passwd):
@@ -42,7 +82,7 @@ def login_post():
 
 # This should be checking for authentication status and if not redirect to the mail page. 
 # It should already be protected by login required decoration but this would catch forceful browseing
-@auth.route('/login')
+@auth.route('/login', methods=['GET'])
 def login():
     if current_user.is_authenticated:
         authnzid=current_user.get_id()
@@ -51,9 +91,10 @@ def login():
         #account=User.query.filter_by(id=sessioncid).first()
         msg='already authenticated' #.format(authnzid.clientname)
         flash(msg)
-        return render_template('home.html')
+        return redirect(url_for('main.presenthome'))
     else:
-        return render_template('index.html')
+        return redirect(url_for('main.index'))
+        #return render_template('index.html')
 
 
 # Utilize Flask builtin user management to allow authenticated users to properly close their sessions
