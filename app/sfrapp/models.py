f9febd33d47d80d86097dc1a25bbbfc7423cfde6
dl21273@essex.ac.uk <dleece@firstfiretech.ca>
2022-02-25 12:07:40 -0700

    extending the  model to include file storage

--- app/sfrapp/models.py
+++ app/sfrapp/models.py
@@ -1,5 +1,6 @@
 from flask_login import UserMixin
 from . import db
+from sqlalchemy.orm import deferred
 
 class User(UserMixin, db.Model):
     __tablename__ = 'userauthnz'
@@ -19,5 +20,21 @@ class DataUser(UserMixin, db.Model):
     useraccessid = db.Column(db.String(12))
     authgroups = db.Column(db.String(60))
     #
-   
+
+class StoredFiles(db.Model):
+    __tablename__ = 'storedfiles'
+    uuid_hex = db.Column(db.String(32), primary_key=True)
+    filename = db.Column(db.String(64))
+    filetype = db.Column(db.String(8))
+    filedata = deferred(db.Column(db.LargeBinary))
+    fileowner = db.Column(db.Integer)
+    authgroups = db.Column(db.String(60))
+    filecreate = db.Column(db.Date)
+    keywords_tags = db.Column(db.String(255))
+    allowupdates = db.Column(db.Boolean)
+    fileversion = db.Column(db.Integer())
+
+
+
+
     
