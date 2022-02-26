253e8323fba470e82e0078872491a480237a572f
dl21273@essex.ac.uk <dleece@firstfiretech.ca>
2022-02-26 09:38:11 -0700

    removed keytest from index

--- app/sfrapp/main.py
+++ app/sfrapp/main.py
@@ -32,7 +32,6 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     print("inside index function")
-    print(SECRET_KEY)
     print(current_user.is_authenticated)
     return render_template('index.html')
 
