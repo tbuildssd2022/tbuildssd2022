ff539d034acff5a509877b184c0694cb40fb8551
dl21273@essex.ac.uk <dleece@firstfiretech.ca>
2022-02-27 20:10:14 -0700

    removing getfileuploadsql module inport

--- app/sfrapp/main.py
+++ app/sfrapp/main.py
@@ -24,7 +24,7 @@ from . import db, getconnectiondata,newdburi
 from . models import DataUser, User, DataGroup
 import io
 # Import custom module classes and functions
-from . tbutility import getauthzfg, getauthzfilesql, getauthzfiles,newresultsdict, getfiledatasql, getfiledata, getmimetype, testfileownersql,testfileownership,getgroupdetails, newsharedgroups,updatesharedgroupssql,updatesharedgrp,testfsradio,getfileextension,testfileextension,getcurdate,getnewuuid,getfileuploadsql,newfileupload,getfiledeletesql
+from . tbutility import getauthzfg, getauthzfilesql, getauthzfiles,newresultsdict, getfiledatasql, getfiledata, getmimetype, testfileownersql,testfileownership,getgroupdetails, newsharedgroups,updatesharedgroupssql,updatesharedgrp,testfsradio,getfileextension,testfileextension,getcurdate,getnewuuid,newfileupload,getfiledeletesql
 
 
 
